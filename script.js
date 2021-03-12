08f2aa44ca0f9753716f5d4fb6267f8a02140684
Vlad <pinokiovs@gmail.com>
2021-03-12 13:53:11 +0200

    add cycle

--- script.js
+++ script.js
@@ -1,20 +1,40 @@
+"use strict";
+
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели', '');
 
 const personalMovieDB = {
-    count: {numberOfFilms},
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+for (let i = 0; i < 2; i++) {
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+if (a != null && b != null && a != '' && b != '' && a.length < 50 && b.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+} else {
+    console.log('Error');
+    i--;
+
+    }
+
+}
+
+if (personalMovieDB.count < 10) {
+    alert("Просмотрено довольно мало фильмов");
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert("Вы классический зритель");
+} else if (personalMovieDB.count >= 30) {
+    alert("Вы киноман");
+} else {
+    alert("Произошла ошибка");
+}
 
 
 console.log(personalMovieDB);
